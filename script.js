34938c5560abeb5c96f6c164aa1b81a12c12f55b
Aniket205kadam <aniketrkadam205@gmail.com>
2025-11-15 12:35:45 +0530

    update portfolio

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
